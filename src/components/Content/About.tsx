7f19c50515b17f496d727945b33ca02855c9b747
marchewazz <mateuszooospot@gmail.com>
2024-03-12 18:45:17 +0100

    Links styling

--- src/components/Content/About.tsx
+++ src/components/Content/About.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faFileArrowDown, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import strings from "../../utilities/strings";
@@ -12,7 +12,7 @@ export default function About() {
         <section>
             <div className="flex justify-between">
                 <div>
-                    <p className="dark:text-gold text-4xl">
+                    <p className="dark:text-gold text-4xl font-semibold">
                         Mateusz <br /> Marchewczyk
                     </p>
                     <p className="dark:text-light-text text-xl">
@@ -22,23 +22,32 @@ export default function About() {
                 <img className="rounded" src="https://picsum.photos/100" alt="" />
             </div>
             <div>
-                <div className="flex justify-evenly text-gold">
-                    <a className="border border-gold rounded hover:w-14" href="mailto:marchewczykmateusz24@gmail.com">
-                        <FontAwesomeIcon icon={faEnvelope} />
+                <div className="flex justify-around text-gold">
+                    <a className="about-link group" href="mailto:marchewczykmateusz24@gmail.com">
+                        <FontAwesomeIcon icon={faEnvelope} className="group-hover:hidden" size="lg" />
+                        <span className="hidden group-hover:block">marchewczykmateusz24@gmail.com</span>
                     </a>
-                    <a className="border border-gold rounded hover:w-14" href="tel:+48730396760">
-                        <FontAwesomeIcon icon={faPhone} />
+                    <a className="about-link group" href="tel:+48730396760">
+                        <FontAwesomeIcon icon={faPhone} className="group-hover:hidden" size="lg" />
+                        <span className="hidden group-hover:block text-nowrap">+48 730 396 760</span>
                     </a>
-                    <a className="border border-gold rounded" href="https://www.linkedin.com/in/mateusz-marchewczyk-b2b7881ba/" target="_blank">
-                        <FontAwesomeIcon icon={faLinkedin} />
+                    <a className="about-link group" href="https://www.linkedin.com/in/mateusz-marchewczyk-b2b7881ba/" target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} size="lg" />
                     </a>
-                    <a className="border border-gold rounded" href="https://github.com/marchewazz" target="_blank">
-                        <FontAwesomeIcon icon={faGithub} />
+                    <a className="about-link group" href="https://github.com/marchewazz" target="_blank">
+                        <FontAwesomeIcon icon={faGithub} className="group-hover:hidden" size="lg" />
+                        <span className="hidden group-hover:block text-nowrap">marchewazz</span>
                     </a>
                 </div>
                 <div className="flex justify-evenly text-gold">
-                    <a className="border border-gold rounded" href={resume} download="cv">{ strings.about.englishCV }</a>
-                    <a className="border border-gold rounded" href={resume} download="cv">{ strings.about.polishCV }</a>
+                    <a className="resume-link group" href={resume} download="cv">
+                        { strings.about.englishCV }
+                        <FontAwesomeIcon icon={faFileArrowDown} className="hidden group-hover:block" size="lg" />
+                    </a>
+                    <a className="resume-link group" href={resume} download="cv">
+                        { strings.about.polishCV }
+                        <FontAwesomeIcon icon={faFileArrowDown} className="hidden group-hover:block" size="lg" />
+                    </a>
                 </div>
             </div>
             <div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ export default {
         'light-text': '#BDBDBF',
         'gold': '#FAB95B',
       },
+      transitionProperty: {
+        multiple: "width , height , backgroundColor , border-radius"
+      }
     },
   },
   plugins: [],
