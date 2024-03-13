b8377409fe75dfd95a2d6d66e3994784764c74a7
marchewazz <mateuszooospot@gmail.com>
2024-03-13 21:42:20 +0100

    Scrollspy

---
+++ src/components/Content/Education.tsx
@@ -0,0 +1,9 @@
+export default function Experience() {
+    return (
+        <section id="education">
+            <h4 className="dark:text-gold text-3xl font-bold text-center">
+                Education
+            </h4>
+        </section>
+    )
+}
--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -13,16 +13,16 @@ export default function NavigationButtons(props: { darkModeChangeFunction: React
         <div ref={props.innerRef} className="absolute text-white flex sm:flex-col w-fit bottom-0 left-0 right-0 mx-auto sm:left-auto sm:right-0 sm:top-1/2 sm:bottom-auto bg-dark-blue z-50 
         border-r border-l border-t md:border-b md:border-r-0 border-white
         rounded-t-3xl sm:rounded-tr-none md:rounded-l-3xl">
-            <button className="navigation-button active rounded-tl-3xl">
+            <button data-to-scrollspy-id="about" className="navigation-button rounded-tl-3xl">
                 <FontAwesomeIcon icon={faUser} />
             </button>
-            <button className="navigation-button">
+            <button data-to-scrollspy-id="experience" className="navigation-button">
                 <FontAwesomeIcon icon={faBriefcase} />
             </button>
-            <button className="navigation-button">
+            <button data-to-scrollspy-id="projects" className="navigation-button">
                 <FontAwesomeIcon icon={faCode} />
             </button>
-            <button className="navigation-button">
+            <button data-to-scrollspy-id="education" className="navigation-button">
                 <FontAwesomeIcon icon={faGraduationCap} />
             </button>
             { strings.getLanguage() == "pl" ? (
@@ -34,7 +34,7 @@ export default function NavigationButtons(props: { darkModeChangeFunction: React
                     <img className="rounded-full" src="https://flagsapi.com/US/shiny/32.png" />
                 </button>
             )}
-            <button className="navigation-button rounded-tr-3xl sm:rounded-tr-none sm:rounded-bl-3xl active" onClick={props.darkModeChangeFunction}>
+            <button className="navigation-button rounded-tr-3xl sm:rounded-tr-none sm:rounded-bl-3xl" onClick={props.darkModeChangeFunction}>
                 <FontAwesomeIcon icon={faMoon} />
             </button>
         </div>
