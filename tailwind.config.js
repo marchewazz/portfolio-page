03ab32121ecc4f40ad85b50794c296c6456f50de
marchewazz <mateuszooospot@gmail.com>
2024-03-13 20:12:51 +0100

    Background blue color updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
     },
     extend: {
       colors: {
-        'main-blue': '#130837',
+        'main-blue': '#080c37',
         'light-blue': '#000358',
         'dark-blue': '#070F2B',
         'light-text': '#BDBDBF',
