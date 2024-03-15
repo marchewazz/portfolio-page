1802164d51240eff6d2100cf59ee89992f8bd72b
marchewazz <mateuszooospot@gmail.com>
2024-03-15 19:58:54 +0100

    Projects mobile layout

--- src/components/Content/Projects/ProjectDisplay.tsx
+++ src/components/Content/Projects/ProjectDisplay.tsx
@@ -14,13 +14,13 @@ export default function ProjectDisplay(props: Readonly<{ project: {
     const [showLinks, setShowLinks] = useState<boolean>(false)
 
     return (
-        <div className="flex py-3 gap-4">
-            <div className="relative lg:w-1/2">
+        <div className="flex flex-col sm:flex-row py-3 gap-4">
+            <div className="relative sm:w-1/2">
                 <div className="absolute shadow-[inset_0_8px_8px_0] shadow-black w-full h-full z-40 rounded-3xl" />
                 <img className="aspect-video border-4 border-black rounded-3xl" src={props.project.screen} alt="" />
             </div>
-            <div className="flex flex-col lg:w-1/2">
-                <p className="text-center text-main-purple dark:text-gold italic font-bold">
+            <div className="flex flex-col sm:w-1/2">
+                <p className="sm:text-center text-main-purple dark:text-gold italic font-bold">
                     { props.project.title }
                 </p>
                 <p className="text-dark-text dark:text-light-text">
